ce882138a13f1839c8e50bebca13d9a7085155d5
@jeremiasbenegaa <jeremias.benega17@gmail.com>
2021-08-27 15:19:54 -0300

    bhashaka

--- ejercicio1/ejercicio2/main.js
+++ ejercicio1/ejercicio2/main.js
@@ -3,19 +3,19 @@
 
 const calcular = () => {
 
-    const datoa = document.getElementById("inp_a").Value;
-    const datob = document.getElementById("inp_b").Value;
-    const datoc = document.getElementById("inp_c").Value;
+    const datoa = document.getElementById("inp_a").value;
+    const datob = document.getElementById("inp_b").value;
+    const datoc = document.getElementById("inp_c").value;
 
     const bloque1 = (Math.pow(datob,2)) - (4*datoa*datoc);
     const raiz =    Math.sqrt(bloque1)
 
-    const r1 = (- datob + raiz) / (2,datoa);
-    const r2 = (- datob - raiz) / (2,datoa);
+    const r1 = (- datob + raiz) / (2*datoa);
+    const r2 = (- datob - raiz) / (2*datoa);
 
     document.getElementById("r1").textContent = "Raiz 1: "+r1;
     document.getElementById("r2").textContent = "Raiz 2: "+r2;
 }
 
-const boton=document.addEventById("btn_calcular");
-boton.addEventListener("Click",calcular);
+const boton=document.getElementById("btn_calcular");
+boton.addEventListener("click",calcular);
